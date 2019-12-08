ba1c6f838c133b06dcfa68706d94b35f5cc488c0
Kunal Harkare <kunal.harkare@hof-university.de>
2019-12-08 01:12:37 +0100

    Added plugins to list Nutritionists and Clients

--- packages/nutrispec_app/ext_localconf.php
+++ packages/nutrispec_app/ext_localconf.php
@@ -25,11 +25,48 @@ call_user_func(
             ]
         );
 
+        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+            'GroupProject.NutrispecApp',
+            'Nutrispec_Nutritionist_List',
+            [
+                'Nutritionist' => 'list, show, new, create, edit, update, delete',
+                'Clients' => 'list, show, new, create, edit, update, delete',
+                'Blog' => 'list, show, new, create, edit, update, delete',
+                'Specialization' => 'list, show, new, create, edit, update, delete',
+                'ClientReport' => 'list, show, new, create, edit, update, delete'
+            ],
+            // non-cacheable actions
+            [
+                'Nutritionist' => 'list, show, new, create, edit, update, delete',
+                'Clients' => 'list, show, new, create, edit, update, delete',
+                'Blog' => 'list, show, new, create, edit, update, delete',
+                'Specialization' => 'list, show, new, create, edit, update, delete',
+                'ClientReport' => 'list, show, new, create, edit, update, delete'
+            ]
+        );
+
+        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+            'GroupProject.NutrispecApp',
+            'Nutrispec_Client_List',
+            [
+                'Clients' => 'list, show, new, create, edit, update, delete',
+                'Nutritionist' => 'list, show, new, create, edit, update, delete',
+                'ClientReport' => 'list, show, new, create, edit, update, delete'
+            ],
+            // non-cacheable actions
+            [
+                'Clients' => 'list, show, new, create, edit, update, delete',
+                'Nutritionist' => 'list, show, new, create, edit, update, delete',
+                'ClientReport' => 'list, show, new, create, edit, update, delete'
+            ]
+        );
+
         // wizards
         \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
             'mod {
                 wizards.newContentElement.wizardItems.plugins {
                     elements {
+                        
                         nutrispec {
                             iconIdentifier = nutrispec_app-plugin-nutrispec
                             title = LLL:EXT:nutrispec_app/Resources/Private/Language/locallang_db.xlf:tx_nutrispec_app_nutrispec.name
@@ -39,18 +76,48 @@ call_user_func(
                                 list_type = nutrispecapp_nutrispec
                             }
                         }
+                        
+                        nutrispec_nutritionist_list {
+                            iconIdentifier = nutrispec_app-plugin-nutrispec_nutritionist_list
+                            title = Nutritionists List
+                            description = Provides a list of all Nutritionists
+                            tt_content_defValues {
+                                CType = list
+                                list_type = nutrispecapp_nutrispec_nutritionist_list
+                            }
+                        }
+                        nutrispec_client_list {
+                            iconIdentifier = nutrispec_app-plugin-nutrispec_client_list
+                            title = Client List
+                            description = Provides a list of all Clients
+                            tt_content_defValues {
+                                CType = list
+                                list_type = nutrispecapp_nutrispec_client_list
+                            }
+                        }
                     }
                     show = *
                 }
            }'
         );
 		$iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
-		
-			$iconRegistry->registerIcon(
-				'nutrispec_app-plugin-nutrispec',
+
+            $iconRegistry->registerIcon(
+                'nutrispec_app-plugin-nutrispec',
+                \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+                ['source' => 'EXT:nutrispec_app/Resources/Public/Icons/user_plugin_nutrispec.svg']
+            );
+
+		    $iconRegistry->registerIcon(
+				'nutrispec_app-plugin-nutrispec_nutritionist_list',
 				\TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
 				['source' => 'EXT:nutrispec_app/Resources/Public/Icons/user_plugin_nutrispec.svg']
 			);
+            $iconRegistry->registerIcon(
+                'nutrispec_app-plugin-nutrispec_client_list',
+                \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+                ['source' => 'EXT:nutrispec_app/Resources/Public/Icons/user_plugin_nutrispec.svg']
+            );
 		
     }
 );
--- packages/nutrispec_app/ext_tables.php
+++ packages/nutrispec_app/ext_tables.php
@@ -11,6 +11,18 @@ call_user_func(
             'NutriSpec'
         );
 
+        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+            'GroupProject.NutrispecApp',
+            'Nutrispec_Nutritionist_List',
+            'Nutritionists List'
+        );
+
+        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+            'GroupProject.NutrispecApp',
+            'Nutrispec_Client_List',
+            'Client List'
+        );
+
         \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('nutrispec_app', 'Configuration/TypoScript', 'NutriSpec App');
 
         \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_nutrispecapp_domain_model_nutritionist', 'EXT:nutrispec_app/Resources/Private/Language/locallang_csh_tx_nutrispecapp_domain_model_nutritionist.xlf');
